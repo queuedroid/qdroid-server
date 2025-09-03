ec8b4c0286986e1b613e8931269e2f37bec19109
Promise Fru <info@promisefru.com>
2025-09-03 16:47:47 +0100

    refactor: remove unused fields from User and Session models

--- models/session.go
+++ models/session.go
@@ -11,10 +11,8 @@ import (
 type Session struct {
 	ID                 uint    `gorm:"primaryKey"`
 	Token              string  `gorm:"size:255;not null;uniqueIndex"`
-	IPAddress          *string `gorm:"size:255;default:null"`
 	IPAddressEncrypted *[]byte `gorm:"size:255;default:null"`
 	IPAddressPseudonym *[]byte `gorm:"size:255;default:null"`
-	UserAgent          *string `gorm:"size:255;default:null"`
 	UserAgentEncrypted *[]byte `gorm:"size:255;default:null"`
 	UserAgentPseudonym *[]byte `gorm:"size:255;default:null"`
 	LastUsedAt         *time.Time
--- models/user.go
+++ models/user.go
@@ -14,14 +14,10 @@ type User struct {
 	ID                   uint    `gorm:"primaryKey"`
 	AccountID            string  `gorm:"size:255;not null;uniqueIndex"`
 	AccountToken         string  `gorm:"size:255;not null;index"`
-	Email                string  `gorm:"size:255;not null;uniqueIndex"`
 	EmailEncrypted       []byte  `gorm:"size:255;not null"`
 	EmailPseudonym       []byte  `gorm:"size:255;not null;uniqueIndex"`
 	Password             string  `gorm:"size:255;not null"`
-	CountryCode          string  `gorm:"size:10;not null"`
 	CountryCodeEncrypted []byte  `gorm:"size:255;not null"`
-	PhoneNumber          *string `gorm:"size:255;default:null"`
-	FullName             *string `gorm:"size:255;default:null"`
 	FullNameEncrypted    *[]byte `gorm:"size:255;default:null"`
 	CreatedAt            time.Time
 	UpdatedAt            time.Time
